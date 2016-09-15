69a93887ea7ec45419e05eb8a0e01becd29c5174
mw1094 <mw1094@georgetown.edu>
2016-09-16 02:00:28 +0800

    Wang_Meng_homwork1
    This is the homework1 completed by Meng Wang in group C

--- Wang_Meng_homework1.py
+++ Wang_Meng_homework1.py
@@ -16,25 +16,37 @@ Homework 1 Group C
 #question1
 dic1={"merry":"god","christmas":"jul","and":"och","happy":"gott","new":"nytt","year":"år"}#creat a dictionary
 def translate(English):
-    '''
+    """
     This function translate a list of English words into Swedish Words.
-    My thought: spilt the string into words, translate words, then reform a string and return it.
-    '''
-    Swedish="" #creat an empty "Swedish"
-    for k in English.split(): #input a string and spilt the string into words. "k" represent each word.
+    My thought: find the matched words in dic and creat a new list.
+    
+    Parameter:
+    A list
+    
+    Returns:
+    A new list consists of translated words
+    """
+    Swedish=[]#creat an empty list "Swedish"
+    for k in English: #input a lsit 'English'
         if k in dic1:
-            Swedish=Swedish+dic1[k]+" " #dic1[k] mathch the keys to their values in dict1. Return the mathced results and add them up to form string
+            Swedish.append(dic1[k]) #dic1[k] mathch the keys to their values in dict1. Return the mathced results and add them up to list.
         else:
-            Swedish=Swedish+k #in case of punctuations
+            Swedish.append(k) #in case of words not in dictionary
     return(Swedish)
     
-print(translate('merry christmas and happy new year'))
+print(translate(['merry','christmas','and','happy','new','year']))
 
 #question2
 def char_freq(string):
     '''
     This function return the frequency listing of characters contained in it.
     My thought: Let letters be the keys and frequency be the values in dictionary.
+    
+    Parameter:
+    A string
+    
+    Returns:
+    A dictionary showing frequency ofcharaters
     '''
     freq={} #freq is a dictionary
     for i in string:
@@ -61,6 +73,12 @@ def encoderdecoder(code):
     This function can encode and decode string using dictionary "key".
     My thought: find the values of every letters in code, and add them up. For punctuations, when can not find 
     keys in "key", keep them and also add them up.
+    
+    Parameter:
+    A string
+    
+    Returns:
+    Decoded string
     '''
     answer="" #creat an empty "answer" 
     for i in code:
@@ -81,6 +99,12 @@ def correct(string):
     character is compressed into one, and 2) inserts an extra space
     after a period if the period is directly followed by a letter.
     My thought: use regular expression, correct the sentence in two steps.
+    
+    Parameter:
+    A string
+    
+    Returns:
+    Correct string
     '''
     string=re.sub('\ +',' ',string) #substitute one or more spaces between two words to one space
     string=re.sub('\.','. ',string) #substitute period end with no space to period end with one space
@@ -96,6 +120,12 @@ def make_3sg_form(word):
     a. If the verb ends in y, remove it and add ies b. If the verb ends in o, ch, s, sh, x
     or z, add es c. By default just add s
     My thought: Use endswith and replace.
+    
+    Parameter:
+    A string(word)
+    
+    Returns:
+    Third person sigular form of the word
     '''
     es=('o','ch','s','sh','x','z')
     if word.endswith('y'):
@@ -119,6 +149,12 @@ def make_ing_form(word):
     a. If the verb ends in e, drop the e and add ing (if not exception: be, see, flee, knee, etc.) b. If the verb ends in ie, change ie to y and
     add ing c. For words consisting of consonant-vowel-consonant, double the final letter before adding ing d. By default just add ing
     My thought: Use replace function.
+    
+    Parameter:
+    A string(word)
+    
+    Returns:
+    Present participle form of the word.
     '''
     exception=['be','see','flee','knee'] #exception, as rule a requires
     if word.endswith('ie'):
@@ -144,6 +180,12 @@ def max_in_list(list1):
     '''
     This function uses the higher order function reduce() to takes a list of numbers and returns the largest
     one.
+    
+    Parameter:
+    A list of numbers
+    
+    Returns:
+    The maximum number
     '''
     return reduce(max,list1) #compare the first two numbers, and then compare the largest number in the first two with the thrid number, and so on.
 
@@ -154,6 +196,12 @@ def listlength(word):
     '''
     The following functions map a list of words into a list of integers
     representing the lengths of the correponding words. Three functions are written using a for-loop.
+    
+    Parameter:
+    A list of words
+    
+    Returns:
+    A new list consists of the lengths of each words
     '''
     length = [] #empty list
     for i in word:
@@ -167,6 +215,12 @@ def listlength(word):
     The following functions map a list of words into a list of integers
     representing the lengths of the correponding words. Three functions are written using the higher order
     function map().
+    
+    Parameter:
+    A list of words
+    
+    Returns:
+    A new list consists of the lengths of each words
     '''
     return list((map(len,word))) #substitute the length of word to word in list using map function
     
@@ -176,6 +230,12 @@ def listlength(word):
     '''
     The following functions map a list of words into a list of integers
     representing the lengths of the correponding words. Three functions are written using list comprehensions.
+    
+    Parameter:
+    A list of words
+    
+    Returns:
+    A new list consists of the lengths of each words
     '''
     length=[len(i) for i in word] #put length of each word in "word" list into "length" list
     return length
@@ -188,6 +248,12 @@ def find_longest_word(list1):
     This function find_longest_word() takes a list of words
     and returns the length of the longest one using only higher order
     functions.
+    
+    Parameter:
+    A list of words
+    
+    Returns:
+    The length of the longest word
     '''
     return max(map(len,list1)) #use map function to substitute list 1 with length of each word in list1, then use max function to find the longest one
 
@@ -195,6 +261,16 @@ print(find_longest_word(['i','love','python']))
 
 #question10
 def filter_long_words(list1, n):
+    '''
+    This function filter_long_words()that takes a list of words and an integer
+    and returns the list of words that are longer than n.
+    
+    Parameter:
+    A list of words and a number n
+    
+    Returns:
+    A list of words that are longer than n
+    '''
     return(list(filter(lambda x:len(x)>n,list1))) #lambda function len(x)>n can see if length of word in list1 >n. filter function pick those words up.
 
 print(filter_long_words(['i','love','python'], 3))
@@ -207,6 +283,12 @@ def translate(list1):
     a function translate() that takes a list of English words and
     returns a list of Swedish words.
     My thought: Use map function to substitute the matched values in list to string.
+    
+    Parameter:
+    A list of words
+    
+    Returns:
+    A list consists of translated words
     '''
     return list(map(dictionary.get,list1))#find matched words in dictionary, substitue words and list them out
 
@@ -217,6 +299,12 @@ def map(function,list1):
     '''
     Implement the higher order functions map()
     Creat a new list, function old list, and add to new list.
+    
+    Parameter:
+    A function and a list
+    
+    Returns:
+    A proceed list using function
     '''
     newlist=[]#creat an empty new list
     for i in list1:
@@ -229,6 +317,12 @@ def filter(function,list1):
     '''
     Implement the higher order functions map(), filter()
     Creat a new list. If function is true, add to new list.
+    
+    Parameter:
+    A function and a list
+    
+    Returns:
+    A list filtered by function
     '''
     newlist=[]#creat a new list
     for i in list1:
@@ -242,6 +336,12 @@ def reduce(function,list1):
     '''
     Implement the higher order functions reduce(). 
     Function i in list and its next.
+    
+    Parameter:
+    A function and a list
+    
+    Returns:
+    A proceed list using function
     '''
     accum=list1[0] #let accum be the first iterator in list1
     for i in list1[1:]: #for every i starting from the second place in list1
